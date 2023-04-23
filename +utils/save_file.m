f0f4ff89cfa0dc9d497e613c0c5ef581e90f293f
Tron <achuan-2@outlook.com>
2023-04-23 18:42:43 +0800

    ✨ 改进 uiputfile 和 uigetfile 函数 使其支持打开设置的默认文件夹，能记住上次选择的路径

--- +utils/save_file.m
+++ +utils/save_file.m
@@ -1,6 +1,13 @@
-function [filename,path]= save_file(fileExtension)
-% fileExtension: {'*.jpg';'*.png';'tif'}
-	f_dummy = figure('Position', [-100 -100 0 0]); %create a dummy figure so that uigetfile doesn't minimize our GUI
-	[filename, path] = uiputfile(fileExtension, 'Save as');
-	delete(f_dummy); %delete the dummy figure
+function [filename,path]= save_file(fileExtension,defaultPath)
+    % fileExtension: {'*.jpg';'*.png';'tif'}
+    
+    f_dummy = figure('Position', [-100 -100 0 0]); %create a dummy figure so that uigetfile doesn't minimize our GUI
+
+    if nargin < 2
+        [filename, path] = uiputfile(fileExtension, 'Save as');
+    else
+        [filename, path] = uiputfile(fileExtension, 'Save as',defaultPath);
+    end
+
+    delete(f_dummy); %delete the dummy figure
 end
--- +utils/select_file.m
+++ +utils/select_file.m
@@ -1,7 +1,13 @@
-function [filename,path]= select_file(fileExtension)
+function [filename,path]= select_file(fileExtension,defaultPath)
 
         f_dummy = figure('Position', [-100 -100 0 0]); %create a dummy figure so that uigetfile doesn't minimize our GUI
-        [filename,path] = uigetfile(fileExtension, 'Open');
+        
+        if nargin < 2
+            [filename,path] = uigetfile(fileExtension, 'Select a file');
+        else
+            [filename,path] = uigetfile(fileExtension, 'Select a file',defaultPath);
+        end
+
         delete(f_dummy); %delete the dummy figure
 
 end
