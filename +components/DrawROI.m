be2c8721b327e2bcd19a82142832d53b1736ad1a
Tron <achuan-2@outlook.com>
2023-11-02 00:58:13 +0800

    ♻️ refactor(DrawROI): 重构为类

--- +components/DrawROI.m
+++ +components/DrawROI.m
@@ -1,11 +1,24 @@
 classdef DrawROI < handle
-    properties
-        
+    properties (SetAccess = public)
+        mask_layer matlab.graphics.primitive.Image
+        mask_alpha double = 0.3 
+        mask (:,:) uint8
+        colored_mask (:,:,3) uint8 
     end
+
+    properties (Dependent, SetAccess = private)
+        binary_mask (:,:) logical
+    end
+
     methods (Hidden)
         function self = DrawROI()
         end
+    end
 
+    methods
+        function result = get.binary_mask(self)
+            result = logical(self.mask);
+        end
     end
 
 end
