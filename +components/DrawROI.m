cb08c43dcf7c2a141fa4d418f5c5dafdda3790e6
Tron <achuan-2@outlook.com>
2023-11-02 02:24:04 +0800

    ♻️ refactor(DrawROI): 添加手动圈选变量

--- +components/DrawROI.m
+++ +components/DrawROI.m
@@ -1,9 +1,26 @@
 classdef DrawROI < handle
+    % 绘制ROI以及手动绘制ROI模块
+    % mask
     properties (SetAccess = public)
         mask_layer matlab.graphics.primitive.Image
-        mask_alpha double = 0.3 
+        mask_opacity double = 0.3 
         mask (:,:) uint8
+        mask_size (:,:) double
         colored_mask (:,:,3) uint8 
+        mask_alphadata
+    end
+    % draw
+    properties
+        current_plot
+        start_drawing
+        start_position
+        last_position
+        n_ROI = 0;
+        strokes
+        current_stroke
+        brush_size = 4;
+        plot_handles;
+        plot_current_handle
     end
 
     properties (Dependent, SetAccess = private)
@@ -18,6 +35,9 @@ classdef DrawROI < handle
     methods
         function result = get.binary_mask(self)
             result = logical(self.mask);
+        end
+        function result = get.mask_alphadata(self)
+            result = self.binary_mask*self.mask_opacity;
         end
     end
 
