8eb9831327a146ca4dc3e3d67997dff778f4e9dc
Tron <achuan-2@outlook.com>
2024-04-22 21:46:37 +0800

    🎨 完善0.1MHZ重建

--- +components/scanimageRealtimeRebuildAvg.m
+++ +components/scanimageRealtimeRebuildAvg.m
@@ -13,13 +13,12 @@ classdef ScanimageRealtimeRebuildAvg < handle
         listeners = cell(1,2); % 监听事件
 
         % 用于绘图
-        figChannels = cell(1,4); % 显示重建图的fig，4个通道是为了兼容
-        rebuildFrameChannels = cell(1,4); % 重建的图
+        fig = cell(1,4); % 显示重建图的fig，4个通道
+        rebuildFrameChannels = cell(1,4); % 重建的图，4个通道
         imageSize; % 采集图像的大小
         countBufferFrames = 0; % 计算当前已经处理多少帧
         firstDrawFlags = [true,true,true,true]; %是否是第一次显示图
         imageShowHandles = cell(1,4); % 用于figure更新图片不闪烁标题
-
         bufferChannels = cell(1,4); % channel的buffer，用于取平均图
 
     end
@@ -29,45 +28,41 @@ classdef ScanimageRealtimeRebuildAvg < handle
             obj.hSI = hSI;
             obj.imageSize = obj.hSI.hRoiManager.pixelsPerLine;
 
-            % TODO：这里的位置可以去三光子电脑具体确认下
-           
-            normalizedPositions = {[0.5471 0.5222 0.1594 0.2833],[0.7094 0.5227 0.1594 0.2833]};
-
-
-
-
-            
+            % 设置fig位置
+            normalizedPositions = {[0.4471 0.5227 0.1594 0.2833],...
+                [0.6094 0.5227 0.1594 0.2833],...
+                [0.8094 0.5227 0.1594 0.2833],...
+                [0.9094 0.5227 0.1594 0.2833]};
+                
+            % 创建fig，默认不可见
             for iChannel = 1:4
-                obj.figChannels{iChannel} = figure('Visible', 'off').empty;
+                obj.fig{iChannel} = figure('Visible', 'off').empty;
             end
 
-            % 根据scanimage的channel数量显示
-
-            for idx = 1:length(obj.hSI.hChannels.channelDisplay)
-
-
-                iChannel = obj.hSI.hChannels.channelDisplay(idx);
-                figPosition =   normalizedPositions{idx};
-                obj.figChannels{iChannel} = figure('Name',sprintf('Channel %d (Processed)',iChannel),'Visible','On',...
-                    'ColorMap',gray(255),'NumberTitle','off','Menubar','none','Tag','image_channel1','Units','normalized','Position',figPosition);
+            % 根据scanimage已打开的channel数量来显示重新fig
+            for i = 1:length(obj.hSI.hChannels.channelDisplay) % 有几个Channel
+                iChannel = obj.hSI.hChannels.channelDisplay(i); %显示第几个channel
+                figPosition =   normalizedPositions{i};
+                
+                obj.fig{iChannel} = figure('Name',sprintf('Channel %d (Processed)',iChannel),'Visible','On',...
+                    'ColorMap',gray(255),'NumberTitle','off','Menubar','none','Units','normalized','Position',figPosition);
                 obj.rebuildFrameChannels{iChannel} =  zeros(obj.imageSize, obj.imageSize);
-
                 obj.bufferChannels{iChannel} =  zeros(10,obj.imageSize, obj.imageSize);
             end
            
         end
 
         function delete(obj)
+            % 删除scanimage对象
             obj.hSI = scanimage.SI.empty;
-
 
             % 删除监听事件
             cellfun(@delete, obj.listeners);
 
             % 关闭图窗
             for iChannel =1:4
-                if isvalid(obj.figChannels{iChannel})
-                    close(obj.figChannels{iChannel})
+                if isvalid(obj.fig{iChannel})
+                    close(obj.fig{iChannel})
                 end
             end
  
@@ -82,7 +77,7 @@ classdef ScanimageRealtimeRebuildAvg < handle
             end
             obj.listeners{1} = addlistener(obj.hSI.hUserFunctions, 'frameAcquired', @obj.countFramesAndSaveToBuffer); % scanimage 每采集一帧，事件发生，调用回调函数
             obj.listeners{2} = addlistener(obj.hSI.hUserFunctions, 'acqAbort', @obj.reset); % scanimage当grab结束或focus abort 事件发生，调用回调函数
-            disp("Listen to scanimage event <frameAcquired>")
+            disp("scanimage_realtime_process：start listening to scanimage event")
         end
 
         function reset(obj,~,~)
@@ -99,14 +94,11 @@ classdef ScanimageRealtimeRebuildAvg < handle
             obj.countBufferFrames = obj.countBufferFrames + 1;
 
             % 平均10帧保存为一张图
+            for i = 1:length(obj.hSI.hChannels.channelDisplay)
+                channelName = obj.hSI.hChannels.channelDisplay(i); 
 
-            for idx = 1:length(obj.hSI.hChannels.channelDisplay)
-                iChannel = idx;
-                channelName = obj.hSI.hChannels.channelDisplay(idx); 
-                %兼容只开ch1、ch3，不开ch2
-                if isvalid(obj.figChannels{channelName})
-                    
-                    obj.processImageChannel(channelIndex = iChannel,channelName=channelName);
+                if isvalid(obj.fig{channelName})       
+                    obj.processImageChannel(channelIndex = i,channelName=channelName);
                 end
             end
 
@@ -133,13 +125,13 @@ classdef ScanimageRealtimeRebuildAvg < handle
                 data = obj.bufferChannels{options.channelName};
                 
                 % adjust contrast accroding to scanimage
-                data = obj.data_rescale(data,options.channelIndex);
+                data = obj.data_rescale(data,options.channelIndex); %把int16数据根据lut变为uint8数据
                 obj.bufferChannels{options.channelName} = data;
                 rebuildFrameData = squeeze(mean(obj.bufferChannels{options.channelName},1));
 
                 % display image
                 obj.displayImageChannel(rebuildFrameData,channel = options.channelName);
-
+                % reset obj.bufferChannels
                 obj.bufferChannels{options.channelName} = zeros(10,obj.imageSize,obj.imageSize);
                 
             end
@@ -152,14 +144,18 @@ classdef ScanimageRealtimeRebuildAvg < handle
                 data
                 options.channel
             end
-            chanDataRescaled = im2uint8(mat2gray(double(data))); % 这一句估计有问题
+            chanDataRescaled = data; % TODO这一句估计有问题
+            % 备注不需要考虑超出255，因为imshow已经限制了[0,255]了
+            % 1. im2uint8(mat2gray(double(data))) 对每一帧单独进行归一化，会导致帧与帧之间的变化
+            % 2. data
+            % 3. data*10
 
 
 
             % 解决标题闪烁的问题: handle = imshow();handle.CData = new_image;
             if obj.firstDrawFlags(options.channel)
 
-                figure(obj.figChannels{options.channel});
+                figure(obj.fig{options.channel});
                 obj.imageShowHandles{options.channel} = imshow(chanDataRescaled,[0,255],'border','tight','InitialMagnification', 'fit');
 
                 obj.firstDrawFlags(options.channel) = false;
